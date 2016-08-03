32f46ac2190cfd2dbe13c31f254b96d51c97fc91
Rachmat Aditiya <raditiya@me.com>
2016-08-03 11:42:04 +0700

    First Commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+	"name": "Penambahan Kolom Sales", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"board",
+		"sale"
+	],
+	"author": "Primeforce", 
+	"category": "Sales", 
+	"description": "Penambahan Field",
+	"data": ["sale.xml"], 
+	"installable": True,
+	"auto_install": False,
+}
---
+++ sale.py
@@ -0,0 +1,9 @@
+from openerp.osv import osv, fields
+
+class sale_order(osv.Model):
+	_name 	= 'sale.order'
+	_inherit= 'sale.order'
+	_columns= {
+		'order_sheet_no' : fields.char("OS/DN No", size=100)
+		'customer_po_no' : fields.char("Nomor PO Customer", size=100)
+	}
