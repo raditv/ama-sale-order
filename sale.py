d2cb158dd0079e0eed868d709c9543d5466d0a32
Rachmat Aditiya <raditiya@me.com>
2016-08-03 11:52:33 +0700

    Bug Fix

--- sale.py
+++ sale.py
@@ -4,6 +4,6 @@ class sale_order(osv.Model):
 	_name 	= 'sale.order'
 	_inherit= 'sale.order'
 	_columns= {
-		'order_sheet_no' : fields.char("OS/DN No", size=100)
+		'order_sheet_no' : fields.char("OS/DN No", size=100),
 		'customer_po_no' : fields.char("Nomor PO Customer", size=100)
 	}
