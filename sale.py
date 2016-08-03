4bc38b613be2b1625cf7a927736b93e68812985b
Rachmat Aditiya <raditiya@me.com>
2016-08-03 12:14:54 +0700

    Format Changed

--- sale.py
+++ sale.py
@@ -4,6 +4,6 @@ class sale_order(osv.Model):
 	_name 	= 'sale.order'
 	_inherit= 'sale.order'
 	_columns= {
-		'order_sheet_no' : fields.char("OS/DN No", size=100),
-		'customer_po_no' : fields.char("Nomor PO Customer", size=100)
+		'order_sheet_no' : fields.char("No. OS/DN", size=100),
+		'customer_po_no' : fields.char("No. PO", size=100)
 	}
